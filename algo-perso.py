53db207b2a554ee3fb98610292c09639efef5a5d
Jeremy js. Sabadie <gpmn013saba>
2022-09-07 11:19:55 +0200

    add len function in the script

--- algo-perso.py
+++ algo-perso.py
@@ -1,14 +1,15 @@
 from random import *
 import statistics
 from math import*
-max=int(input("le maximum des tirages sera de?"))
-min=int(input("le minimum des tirages sera de?"))
+max=int(input("la valeur maximale des tirages sera de?"))
+min=int(input("la valeur minimale des tirages sera de?"))
 tab=[]
-x=int(input("entrez le nombre de tour de boucle:"))
+x=int(input("entrez le nombre d'items à stocker:"))
 for loop in range(x):
   n = randint(min,max)
    
-  print("le nombre tiré est :"+str(n))
+  
   tab.append(n)
-x = statistics.mean(tab)
-print("la moyenne des tirages est de : "+str(x)+"avec un minimum de: "+str(min)+"et un maximum de : "+str(max))
+mean = statistics.mean(tab)
+size= len(tab)
+print("la moyenne des tirages est de : "+str(mean)+"avec un minimum de: "+str(min)+"et un maximum de : "+str(max)+" dans une liste de: "+str(size)+" items.")
